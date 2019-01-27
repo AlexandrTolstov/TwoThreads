32e0659c11a4f3bbb1d6542bc5ffaf7dbb677328
AlexandrTolstov <alextav_85@mail.ru>
2019-01-27 18:43:12 +0600

    1

--- TwoThreads/Program.cs
+++ TwoThreads/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TwoThreads
@@ -10,6 +11,26 @@ namespace TwoThreads
     {
         static void Main(string[] args)
         {
+            Thread myThread = new Thread(Count);
+            myThread.Start();
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine("Главный поток:");
+                Console.WriteLine(i);
+                Thread.Sleep(300);
+            }
+            Console.ReadLine();
+        }
+
+        private static void Count()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine("Второй поток:");
+                Console.WriteLine(i);
+                Thread.Sleep(400);
+            }
         }
     }
 }
